09ba9f0348eb4ece679f97f05456be1e6101c074
imadB <berrahil.imad2@gmail.com>
2021-08-01 01:32:19 -0400

    premier test

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test1</title>
+	<link rel="stylesheet" type="text/css" href="./css/index.css">
+</head>
+<body>
+<section>
+	<h2> ANIMATION DE TEST2</h2>
+</section>
+</body>
+</html>
